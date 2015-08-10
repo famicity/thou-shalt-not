17ce3ec50fc434dcb5a2afcea6f9120cd16face3
Nicolas Barbotte <nicolas@famicity.com>
2015-08-10 10:05:32 +0200

    Thou shalt add es5 strict mode

---
+++ es5/strict.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  rules: {
+    'dot-notation': 2,
+    'no-else-return': 2,
+    'no-eq-null': 2,
+    'handle-callback-err': 2,
+    'no-inline-comments': 2,
+    'one-var': [2, 'never'],
+    'quote-props': [2, 'as-needed'],
+    'spaced-comment': [2, 'always'],
+  },
+  extends: ['thou-shalt-not']
+};

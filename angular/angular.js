d44ae39081855cb9023db3c65765b8c214ecdd5d
Nicolas Barbotte <nicolas@famicity.com>
2015-09-23 09:26:01 +0200

    update angular rules

--- angular/angular.js
+++ angular/angular.js
@@ -2,12 +2,23 @@
 
 module.exports = {
   rules: {
-    'angular/ng_controller_as': 0,
-    'angular/ng_controller_as_route': 0,
-    'angular/ng_controller_as_vm': 0,
-    'angular/ng_directive_name': [2, 'fc'],
-    'angular/ng_window_service': 1,
-    'angular/ng_document_service': 1
+    'angular/directive-name': [2, 'fc'],
+    'angular/angularelement': 2,
+    'angular/no-jquery-angularelement': 2,
+    'angular/interval-service': 2,
+    'angular/timeout-service': 2,
+    'angular/module-getter': 2,
+    'angular/module-setter': 2,
+    'angular/no-http-callback': 2
+    'angular/component-limit': [2, 1],
+    'angular/controller-as': 1,
+    'angular/controller-as-route': 1,
+    'angular/window-service': 1,
+    'angular/document-service': 1,
+    'angular/deferred': 1,
+    'angular/function-type': [1, 'named'],
+    'angular/no-controller': 1,
+    'angular/controller-as-vm': 0,
   },
   extends: ['thou-shalt-not', 'angular']
 };

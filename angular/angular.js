1bbc50cec2d4d9031453d26d02955c28e572b10f
Nicolas Barbotte <nicolas@famicity.com>
2015-09-23 09:28:16 +0200

    fix ','s

--- angular/angular.js
+++ angular/angular.js
@@ -9,7 +9,7 @@ module.exports = {
     'angular/timeout-service': 2,
     'angular/module-getter': 2,
     'angular/module-setter': 2,
-    'angular/no-http-callback': 2
+    'angular/no-http-callback': 2,
     'angular/component-limit': [2, 1],
     'angular/controller-as': 1,
     'angular/controller-as-route': 1,
@@ -18,7 +18,7 @@ module.exports = {
     'angular/deferred': 1,
     'angular/function-type': [1, 'named'],
     'angular/no-controller': 1,
-    'angular/controller-as-vm': 0,
+    'angular/controller-as-vm': 0
   },
   extends: ['thou-shalt-not', 'angular']
 };

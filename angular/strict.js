af207f09df9e19c3da6b3a2309f67c1dd98338ec
Nicolas Barbotte <nicolas@famicity.com>
2016-01-04 12:05:34 +0100

    use new rule names

--- angular/strict.js
+++ angular/strict.js
@@ -2,10 +2,10 @@
 
 module.exports = {
   rules: {
-    'angular/ng_controller_as': 2,
-    'angular/ng_controller_as_route': 2,
-    'angular/ng_window_service': 2,
-    'angular/ng_document_service': 2
+    'angular/controller_as': 2,
+    'angular/controller_as_route': 2,
+    'angular/window_service': 2,
+    'angular/document_service': 2
   },
   extends: ['thou-shalt-not/angular']
 };
